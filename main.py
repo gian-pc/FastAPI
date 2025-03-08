2d4cde5a8b21aeac1cb145123ac9c7bc429b2f77
gianpc <gpaucarcortez@gmail.com>
2025-03-08 00:50:53 -0500

    feat: añadir endpoint para obtener items en main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "mundo"}
+
+
+# http://127.0.0.1:8000/items/1000?q=5
+@app.get('/items/{item_id}')
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
