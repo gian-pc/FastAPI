f20810ee2ec5f64cd5726e2be5f213b6f9747348
gianpc <gpaucarcortez@gmail.com>
2025-03-09 21:37:17 -0500

    feat: añadir endpoint PUT para actualizar items en main.py usando Pydantic

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
 def read_root():
@@ -16,3 +24,7 @@ def read_item(item_id: int, q: str = None):
 @app.get('/calculadora')
 def calcular(operando_1:float, operando_2:float):
     return {'suma': operando_1 + operando_2}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
